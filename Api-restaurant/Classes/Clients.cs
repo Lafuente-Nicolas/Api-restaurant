eb6ea359ad73b15b58aab8e9306e87964c2254dc
Loicwr <Nephtyse19@hotmail.fr>
2025-07-16 14:23:44 +0200

    feat : add the new class clients

---
+++ Api-restaurant/Classes/Clients.cs
@@ -0,0 +1,16 @@
+﻿namespace Api_restaurant.Classes
+{
+    public class Clients
+    {
+        public int Id { get; set; }
+        public string? Nom { get; set; }
+        public string? Prenom { get; set; }
+        public int Telephone { get; set; }
+        public string Email { get; set; }
+        public int Numero_de_rue { get; set; }
+        public string Nom_de_rue { get; set; }
+        public int Code_postal { get; set; }
+        public string Ville { get; set; }
+        public string? Categorie { get; set; }
+    }
+}
--- Api-restaurant/Classes/articles.cs
+++ Api-restaurant/Classes/articles.cs
@@ -2,6 +2,7 @@
 {
     public class articles
     {
+        public int Id { get; set; }
         public string? Nom { get; set; }
         public int Prix { get; set; }
         public string? Categorie { get; set; }
