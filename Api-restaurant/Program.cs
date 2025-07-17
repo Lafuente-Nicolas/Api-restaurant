6ce64f49748ad6699aa4754fdbaee2c1cce4e3eb
abdellah59 <abdellah.ait59@gmail.com>
2025-07-17 11:39:49 +0200

    feat: add restaurnatdb to ccreat database

--- Api-restaurant/Program.cs
+++ Api-restaurant/Program.cs
@@ -1,6 +1,27 @@
+using Api_restaurant.Classes;
+using Api_restaurant.Data;
+using Microsoft.EntityFrameworkCore;
+using Api_restaurant.Dto;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<RestaurantDb>(options =>
+    options.UseSqlite("Data Source=restaurant.db"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+
+/*var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+*/
 app.Run();
